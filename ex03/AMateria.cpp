67e890b20a6e116e4a38a159db3abee911c2392f
Aaron Padilla <aapadill@student.hive.fi>
2025-05-15 20:31:40 +0300

    AMateria methods defined?

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -13,3 +13,34 @@
 #include "AMateria.hpp"
 #include "ICharacter.hpp"
 #include <iostream>
+
+AMateria::AMateria(std::string const &type) : _type(type)
+{
+}
+
+AMateria::AMateria(AMateria const &other): _type(other._type)
+{
+}
+
+AMateria &AMateria::operator=(AMateria const &other)
+{
+	if (this != &other)
+	{
+		_type = other._type;
+	}
+	return *this;
+}
+
+AMateria::~AMateria()
+{
+}
+
+std::string const &AMateria::getType() const
+{
+	return _type;
+}
+
+void AMateria::use(ICharacter &target)
+{
+	std::cout << "* uses generic materia on" << target.getName() << " *" << std::endl;
+}
